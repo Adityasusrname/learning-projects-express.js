afe8ee913912ca864ad8e69275750e06304105a2
Aditya Srivastava <adityasmailbooxx@gmail.com>
2022-08-05 19:22:03 +0530

    Controller getAllTasks added

--- Task Manager/routes/tasks.js
+++ Task Manager/routes/tasks.js
@@ -1,12 +1,10 @@
 const express = require('express')
 const router = express.Router()
 
+const {getAllTasks} = require('../controllers/tasks')
 
-router.route('/').get((req,res)=>{
 
-res.send('All items')
-
-})
+router.route('/').get(getAllTasks)
 
 module.exports= router
 
