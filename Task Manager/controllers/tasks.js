a026883d6d221c93d1f70eac126943dd41ddeb47
Aditya Srivastava <adityasmailbooxx@gmail.com>
2022-08-06 17:09:37 +0530

    getAllTasks and createNewTask working

--- Task Manager/controllers/tasks.js
+++ Task Manager/controllers/tasks.js
@@ -1,12 +1,32 @@
-const getAllTasks = (req,res)=>{
+const Task = require('../models/Task')
 
-    res.send('All items from the file')
-    
+const getAllTasks = async (req,res)=>{
+
+    try {
+
+        const tasks = await Task.find({})
+        res.status(200).json({tasks})
+
+    } catch (error) {
+
+        res.status(500).json({msg:error})
+        
     }
+ }
 
-const createNewTask = (req,res)=>{
+const createNewTask = async (req,res)=>{
 
-res.send('Post request working')
+try {
+
+    const task=await Task.create(req.body)
+    res.status(201).json({task})
+    
+} catch (error) {
+    res.status(500).json({msg:error})
+}
+
+
+
 
 }
 
