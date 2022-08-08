f723abcc39f942397aaf0b54bbaf5754a3f33ee9
Aditya Srivastava <adityasmailbooxx@gmail.com>
2022-08-09 00:52:39 +0530

    patchTask working

--- Task Manager/controllers/tasks.js
+++ Task Manager/controllers/tasks.js
@@ -47,9 +47,22 @@ try {
 
 }
 
-const patchTask = (req,res)=>{
+const patchTask = async (req,res)=>{
 
-res.send(`Task ${req.params.id} patched!`)
+try {
+    const {id:taskID} = req.params
+    const task = await Task.findOneAndUpdate({_id:taskID},req.body,{
+        new:true,
+        runValidators:true
+    })
+    if(!task){
+        return res.status(404).json({msg:`No task with id:${taskID}`})
+    }
+    res.status(200).json({task})
+} catch (error) {
+    res.status(500).json({msg:error})
+    
+}
 
 }
 
